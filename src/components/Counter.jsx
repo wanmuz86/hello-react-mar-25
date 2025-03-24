d12c777b7627e24351a95e4f8595e5fc3a0e175b
Wan Muzaffar <wanmuz86@gmail.com>
2025-03-24 15:01:44 +0700

    add message and setMessage state

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -7,17 +7,25 @@ const Counter = () => {
 // and setCounter as setter
 // The initial value is set to 0
 const [count, setCount] = useState(0);
+const [message, setMessage] = useState('');
 
 // Increment function that will increment the value of count by 1
 const increment = () => {
     setCount(count + 1);
+    setMessage('');
 }
 const decrement = () => {
+    if (count === 0){
+        setMessage('Number cannot be less than 0')
+    }
+    else {
     setCount(count-1)
+    }
 }
 
 const reset = () => {
     setCount(0)
+    setMessage('');
 }
   return (
     <div>
@@ -26,6 +34,7 @@ const reset = () => {
         <button onClick={decrement}>Decrement!</button>
         <button onClick={reset}>Reset</button>
         <h3>Current Count: {count}</h3>
+        <p>{message}</p>
     </div>
   )
 }
