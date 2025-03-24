bf2b1af75eef7b94c707e2df3c4d680d581fcb85
Wan Muzaffar <wanmuz86@gmail.com>
2025-03-24 14:50:51 +0700

    add setCount for the buttons/onClick

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,17 +1,30 @@
 import React,{useState} from 'react'
 
+
 const Counter = () => {
 
 // Create a "state"/variable called it count (getter)
 // and setCounter as setter
 // The initial value is set to 0
-const [count, setCount] = useState(20);
+const [count, setCount] = useState(0);
 
+// Increment function that will increment the value of count by 1
+const increment = () => {
+    setCount(count + 1);
+}
+const decrement = () => {
+    setCount(count-1)
+}
+
+const reset = () => {
+    setCount(0)
+}
   return (
     <div>
-        <button>Increment!</button>
-        <button>Decrement!</button>
-        <button>Reset</button>
+        {/* onClick for React needs to be camelCase onClick */}
+        <button onClick={increment}>Increment!</button>
+        <button onClick={decrement}>Decrement!</button>
+        <button onClick={reset}>Reset</button>
         <h3>Current Count: {count}</h3>
     </div>
   )
