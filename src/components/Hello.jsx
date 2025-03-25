bb272048990749a4bfaf27de09ac891ba1644710
Wan Muzaffar <wanmuz86@gmail.com>
2025-03-25 08:23:52 +0700

    example of conditional rendering

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Counter from './Counter'
 // Shortcut for the extension:
 //rafce - > react arrow function component with export
 // rfce -> react function component with export
 // rcc -> React class component with export
 const Hello = () => {
+
+  const [age, setAge] = useState(16);
+  
   return (
     <div>
     <h1>Hello World</h1>
@@ -19,6 +22,18 @@ const Hello = () => {
      <Counter min={-5} initial={0} max={5}/>
      <hr />
      <Counter min={-2} initial={3} max={10}/>
+     <hr/>
+     <h2>Example of Conditional Rendering</h2>
+      <p>Here is an example of conditional rendering</p>
+      <p>Age: {age}</p>
+      {
+        // Conditional Rendering  - A certain component will only render if a certain condition is met
+        // Example Scenario  - API Call, retrieving data from a database, etc. we will show a loading spinner
+        // We show data when retrieving the dta is done and spinner during loading
+        // Some will call it ternary operator
+        age >= 18 ? <p>You are an adult</p> : <p>You are not on a legal age</p> 
+      }
+      <button onClick={()=> setAge(Math.ceil(Math.random() * 40))}>Generate random age</button>
     </div>
   )
 }
